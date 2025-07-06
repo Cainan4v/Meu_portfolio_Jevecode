57cd335bfe9646ee2aa21e688b04664ba4097c26
cainan <cainan.v07@gmail.com>
2025-07-06 18:18:44 -0300

    adicionado botao dark

--- js/index.js
+++ js/index.js
@@ -30,3 +30,69 @@ function ChamarMenu() {
   const isVisible = sidebar.style.right === "0px";
   toggleSidebar(!isVisible); // Alterna visibilidade
 }
+
+
+const words = ["Modernas|", "Responsivas|", "Funcionais|"];
+const wordElement = document.querySelector('.word');
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function type() {
+  const currentWord = words[wordIndex];
+  const displayed = currentWord.substring(0, charIndex);
+
+  wordElement.textContent = displayed;
+
+  if (isDeleting) {
+    charIndex--;
+  } else {
+    charIndex++;
+  }
+
+  let typeSpeed = isDeleting ? 60 : 150;
+
+  if (!isDeleting && charIndex === currentWord.length) {
+    typeSpeed = 1500; // tempo de pausa com a palavra completa
+    isDeleting = true;
+  } else if (isDeleting && charIndex === 0) {
+    isDeleting = false;
+    wordIndex = (wordIndex + 1) % words.length;
+    typeSpeed = 500; // pausa antes de começar nova palavra
+  }
+
+  setTimeout(type, typeSpeed);
+}
+
+// iniciar a animação
+type();
+
+function darkMode() {
+  const body = document.body;
+  const btnImage = document.getElementById("bdl");
+  const btndark = document.getElementById('btn-dark');
+
+  body.classList.toggle('dark-theme');
+  btndark.classList.remove('btn-anim');
+  void btndark.offsetWidth;
+  btndark.classList.add('btn-anim')
+
+  function timeD() {
+     if (body.classList.contains('dark-theme')) {
+     btnImage.src = 'img/moon.png';
+   } else {
+     btnImage.src = 'img/sol.png';
+   }
+  }
+  tempo = setTimeout( timeD, 300);
+
+  const itemImg = document.querySelectorAll('.item');
+
+  itemImg.forEach(img => {
+    const darkSrc = img.getAttribute('data-dark');
+    const lightSrc = img.getAttribute('data-light');
+    img.src = body.classList.contains('dark-theme') ? darkSrc : lightSrc;
+  });
+
+  
+}
